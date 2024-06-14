8e928e1b4a73056d136b8ab293bf7ee82d52520a
ErtugrulHanBarbaros <115144315+snypr4151@users.noreply.github.com>
2024-06-14 15:42:06 +0300

    Update mainApi.js

--- mainApi.js
+++ mainApi.js
@@ -25,7 +25,7 @@ const contract = new web3.eth.Contract(contractAbi, contractAddress);
 const pool = new Pool({
     user: 'postgres',
     host: 'localhost',
-    database: 'Kullanicilar',
+    database: 'kullanicilar',
     password: 'snypr4151',
     port: 5432,
 });
@@ -48,8 +48,6 @@ function authenticateToken(req, res, next) {
 app.post('/registerInstitution', async (req, res) => {
     const { kurumAdi, telefon, adres, sektor, sifre } = req.body;
     try {
-        console.log('Register request received:', req.body); // Hata ayıklama bilgisi
-
         // Kurum adı kontrolü
         const existingInstitution = await pool.query(
             `SELECT * FROM institutions WHERE kurumAdi = $1`,
@@ -62,7 +60,6 @@ app.post('/registerInstitution', async (req, res) => {
         }
 
         const hashedPassword = await bcrypt.hash(sifre, 10);
-        console.log('Hashed password:', hashedPassword); // Hata ayıklama bilgisi
 
         const newInstitution = await pool.query(
             `INSERT INTO institutions (kurumAdi, telefon, adres, sektor, sifre)
@@ -70,10 +67,10 @@ app.post('/registerInstitution', async (req, res) => {
             [kurumAdi, telefon, adres, sektor, hashedPassword]
         );
 
-        console.log('New institution added:', newInstitution.rows[0]); // Hata ayıklama bilgisi
+        console.log('New institution added:', newInstitution.rows[0]);
         res.json({ success: true, institution: newInstitution.rows[0] });
     } catch (error) {
-        console.error('Error registering institution:', error); // Hata ayıklama bilgisi
+        console.error('Error registering institution:', error); 
         res.status(500).json({ success: false, message: 'Kurum eklenirken bir hata oluştu.', error: error.message });
     }
 });
@@ -102,44 +99,6 @@ app.post('/loginInstitution', async (req, res) => {
     }
 });
 
-// Register User
-app.post('/registerUser', async (req, res) => {
-    const { username, password, kurumId } = req.body;
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = await pool.query(
-            `INSERT INTO users (username, password, kurumId)
-            VALUES ($1, $2, $3) RETURNING *`,
-            [username, hashedPassword, kurumId]
-        );
-        res.json({ success: true, user: newUser.rows[0] });
-    } catch (error) {
-        res.status(500).json({ success: false, message: 'Kullanıcı eklenirken bir hata oluştu.', error: error.message });
-    }
-});
-
-// Login User
-app.post('/loginUser', async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        const user = await pool.query(
-            `SELECT * FROM users WHERE username = $1`,
-            [username]
-        );
-        if (user.rows.length === 0) {
-            return res.status(401).json({ success: false, message: 'Kullanıcı adı veya şifre hatalı.' });
-        }
-        const validPassword = await bcrypt.compare(password, user.rows[0].password);
-        if (!validPassword) {
-            return res.status(401).json({ success: false, message: 'Kullanıcı adı veya şifre hatalı.' });
-        }
-        const token = jwt.sign({ id: user.rows[0].id, type: 'user' }, JWT_SECRET, { expiresIn: '1h' });
-        res.json({ success: true, message: 'Başarıyla giriş yapıldı', token });
-    } catch (error) {
-        res.status(500).json({ success: false, message: 'Giriş işlemi sırasında bir hata oluştu.', error: error.message });
-    }
-});
-
 // Get Profile
 app.get('/profil', authenticateToken, async (req, res) => {
     try {
@@ -158,18 +117,16 @@ app.get('/profil', authenticateToken, async (req, res) => {
 app.post('/diplomaEkle', authenticateToken, async (req, res) => {
     const { tcNo, ad, soyad, kurumId, mezuniyetTarihi } = req.body;
     try {
-        console.log('Diploma ekleme isteği alındı:', req.body); // Hata ayıklama bilgisi
-
         const defaultAccount = await web3.eth.getAccounts().then(accounts => accounts[0]);
-        console.log('Varsayılan hesap:', defaultAccount); // Hata ayıklama bilgisi
+        console.log('Varsayılan hesap:', defaultAccount); 
 
         const options = { from: defaultAccount };
         const tx = await contract.methods.DiplomaEkle(tcNo, ad, soyad, kurumId, mezuniyetTarihi).send(options);
 
-        console.log('İşlem gönderildi:', tx); // Hata ayıklama bilgisi
+        console.log('İşlem gönderildi:', tx); 
         res.json({ success: true, message: 'Diploma başarıyla eklendi.' });
     } catch (error) {
-        console.error('Diploma eklenirken hata oluştu:', error); // Hata ayıklama bilgisi
+        console.error('Diploma eklenirken hata oluştu:', error);
         res.status(500).json({ success: false, message: 'Diploma eklenirken bir hata oluştu.', error: error.message });
     }
 });
@@ -177,11 +134,11 @@ app.post('/diplomaEkle', authenticateToken, async (req, res) => {
 
 // Add Certificate
 app.post('/sertifikaEkle', authenticateToken, async (req, res) => {
-    const { tcNo, ad, soyad, kurumId, verilmeTarihi } = req.body;
+    const { tcNo, ad, soyad, kurumId, verilmeTarihi,sertifikaAdi  } = req.body;
     try {
         const defaultAccount = await web3.eth.getAccounts().then(accounts => accounts[0]);
         const options = { from: defaultAccount };
-        await contract.methods.SertifikaEkle(tcNo, ad, soyad, kurumId, verilmeTarihi).send(options);
+        await contract.methods.SertifikaEkle(tcNo, ad, soyad, kurumId, verilmeTarihi,sertifikaAdi ).send(options);
         res.json({ success: true, message: 'Sertifika başarıyla eklendi.' });
     } catch (error) {
         res.status(500).json({ success: false, message: 'Sertifika eklenirken bir hata oluştu.', error: error.message });
@@ -192,11 +149,25 @@ app.post('/sertifikaEkle', authenticateToken, async (req, res) => {
 app.post('/diplomaOnayla', authenticateToken, async (req, res) => {
     const { tcNo } = req.body;
     try {
+        const data = await contract.methods.DiplomaSorgula(tcNo).call();
+        const kurumId = parseInt(data[3], 10);
+
+        if (req.user.id !== kurumId) {
+            return res.status(403).json({ success: false, message: 'Bu diploma üzerinde onaylama yetkiniz yok.' });
+        }
+
+        if (!tcNo || typeof tcNo !== 'string' || tcNo.trim().length === 0) {
+            return res.status(400).json({ success: false, message: 'Geçersiz TC No.' });
+        }
+
         const defaultAccount = await web3.eth.getAccounts().then(accounts => accounts[0]);
+        console.log('Varsayılan hesap:', defaultAccount);
+
         const options = { from: defaultAccount };
         await contract.methods.DiplomaOnayla(tcNo).send(options);
         res.json({ success: true, message: 'Diploma başarıyla onaylandı.' });
     } catch (error) {
+        console.error('Diploma onaylanırken bir hata oluştu:', error);
         res.status(500).json({ success: false, message: 'Diploma onaylanırken bir hata oluştu.', error: error.message });
     }
 });
@@ -205,6 +176,13 @@ app.post('/diplomaOnayla', authenticateToken, async (req, res) => {
 app.post('/sertifikaOnayla', authenticateToken, async (req, res) => {
     const { tcNo } = req.body;
     try {
+        const data = await contract.methods.SertifikaSorgula(tcNo).call();
+        const kurumId = parseInt(data[4], 10);
+
+        if (req.user.id !== kurumId) {
+            return res.status(403).json({ success: false, message: 'Bu sertifika üzerinde onaylama yetkiniz yok.' });
+        }
+
         const defaultAccount = await web3.eth.getAccounts().then(accounts => accounts[0]);
         const options = { from: defaultAccount };
         await contract.methods.SertifikaOnayla(tcNo).send(options);
@@ -215,11 +193,14 @@ app.post('/sertifikaOnayla', authenticateToken, async (req, res) => {
 });
 
 // Query Diploma
-app.get('/diplomaSorgula/:tcNo', authenticateToken, async (req, res) => {
+app.post('/diplomaSorgula', authenticateToken, async (req, res) => {
+    const { tcNo } = req.body; // body içinden TC Kimlik Numarası alınıyor
     try {
-        const { tcNo } = req.params;
+        // TC Kimlik Numarası'na göre diploma sorgulama işlemi yapılıyor
         const data = await contract.methods.DiplomaSorgula(tcNo).call();
+        // Sonucu JSON formatında gönderme
         res.json({
+            success: true,
             TcNo: data[0],
             Ad: data[1],
             Soyad: data[2],
@@ -228,23 +209,27 @@ app.get('/diplomaSorgula/:tcNo', authenticateToken, async (req, res) => {
             MezuniyetTarihi: data[5]
         });
     } catch (error) {
+        console.log('Diploma sorgulanırken bir hata oluştu:', error.message)
         console.error('Diploma sorgulanırken bir hata oluştu:', error);
         res.status(500).json({ success: false, message: 'Diploma sorgulanırken bir hata oluştu.', error: error.message });
     }
 });
 
+
 // Query Certificate
-app.get('/sertifikaSorgula/:tcNo', authenticateToken, async (req, res) => {
+app.post('/sertifikaSorgula', authenticateToken, async (req, res) => {
+    const { tcNo } = req.body;
     try {
-        const { tcNo } = req.params;
         const data = await contract.methods.SertifikaSorgula(tcNo).call();
         res.json({
+            success: true,
             TcNo: data[0],
             Ad: data[1],
             Soyad: data[2],
             KurumId: data[3],
             isState: data[4],
-            VerilmeTarihi: data[5]
+            VerilmeTarihi: data[5],
+            SertifikaAdi: data[6]
         });
     } catch (error) {
         res.status(500).json({ success: false, message: 'Sertifika sorgulanırken bir hata oluştu.', error: error.message });
@@ -256,6 +241,10 @@ app.post('/diplomaGuncelle', authenticateToken, async (req, res) => {
     const { tcNo, ad, soyad, kurumId, mezuniyetTarihi } = req.body;
     try {
         const defaultAccount = await web3.eth.getAccounts().then(accounts => accounts[0]);
+        const diploma = await contract.methods.DiplomaSorgula(tcNo).call();
+        if (diploma[3] !== req.user.id.toString()) {
+            return res.status(403).json({ success: false, message: 'Sadece ilgili kurum diplomasını güncelleyebilir.' });
+        }
         const options = { from: defaultAccount };
         await contract.methods.DiplomaGuncelle(tcNo, ad, soyad, kurumId, mezuniyetTarihi).send(options);
         res.json({ success: true, message: 'Diploma başarıyla güncellendi.' });
@@ -266,11 +255,15 @@ app.post('/diplomaGuncelle', authenticateToken, async (req, res) => {
 
 // Update Certificate
 app.post('/sertifikaGuncelle', authenticateToken, async (req, res) => {
-    const { tcNo, ad, soyad, kurumId, verilmeTarihi } = req.body;
+    const { tcNo, ad, soyad, kurumId, verilmeTarihi,sertifikaAdi  } = req.body;
     try {
         const defaultAccount = await web3.eth.getAccounts().then(accounts => accounts[0]);
+        const sertifika = await contract.methods.SertifikaSorgula(tcNo).call();
+        if (sertifika[3] !== req.user.id.toString()) {
+            return res.status(403).json({ success: false, message: 'Sadece ilgili kurum sertifikayı güncelleyebilir.' });
+        }
         const options = { from: defaultAccount };
-        await contract.methods.SertifikaGuncelle(tcNo, ad, soyad, kurumId, verilmeTarihi).send(options);
+        await contract.methods.SertifikaGuncelle(tcNo, ad, soyad, kurumId, verilmeTarihi,sertifikaAdi ).send(options);
         res.json({ success: true, message: 'Sertifika başarıyla güncellendi.' });
     } catch (error) {
         res.status(500).json({ success: false, message: 'Sertifika güncellenirken bir hata oluştu.', error: error.message });
